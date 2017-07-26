13ed1d2d34b7bb43dbefc8a827bd00fb34261ff6
Joel Epstein <joel.epstein2@gmail.com>
2017-07-26 15:36:48 -0700

    changed constructor function a little

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 'use strict'
 
-function projectConstructor(imageName, imagePath, projectName, projectPath, skills) {
-  this.imageName = imageName;
-  this.imagePath = imagePath;
+function projectConstructor(projectName, projectPath, skills, imageName, imagePath) {
   this.projectName = projectName;
   this.projectPath = projectPath;
   this.skills = skills;
+  this.imageName = imageName;
+  this.imagePath = imagePath;
 }
