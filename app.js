69eda98121ebb8130f6038b9e4e9612e48dc81cf
Joel Epstein <joel.epstein2@gmail.com>
2017-07-26 15:34:45 -0700

    added JS constructor but I think it still needs work...

---
+++ app.js
@@ -0,0 +1,9 @@
+'use strict'
+
+function projectConstructor(imageName, imagePath, projectName, projectPath, skills) {
+  this.imageName = imageName;
+  this.imagePath = imagePath;
+  this.projectName = projectName;
+  this.projectPath = projectPath;
+  this.skills = skills;
+}
