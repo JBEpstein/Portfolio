e7ed150002e28929c3c77a2f0a0fef44189bb74b
Joel Epstein <joel.epstein2@gmail.com>
2017-09-05 18:20:44 -0700

    fixing stuff

--- public/scripts/project.js
+++ public/scripts/project.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function Article (rawDataObj) {
+function Project (rawDataObj) {
   this.author = rawDataObj.author;
   this.authorUrl = rawDataObj.authorUrl;
   this.title = rawDataObj.title;
@@ -9,10 +9,10 @@ function Article (rawDataObj) {
   this.publishedOn = rawDataObj.publishedOn;
 }
 
-Article.all = [];
+Project.all = [];
 
-Article.prototype.toHtml = function() {
-  let template = Handlebars.compile($('#article-template').text());
+Project.prototype.toHtml = function() {
+  let template = Handlebars.compile($('#Project-template').text());
 
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
@@ -21,38 +21,34 @@ Article.prototype.toHtml = function() {
   return template(this);
 };
 
-Article.loadAll = function(rawData) {
+Project.loadAll = function(rawData) {
   rawData.sort(function(a,b) {
     return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
   });
 
   rawData.forEach(function(ele) {
-    Article.all.push(new Article(ele));
+    Project.all.push(new Project(ele));
   })
 }
 
-Article.fetchAll = function() {
+Project.fetchAll = function() {
   if (localStorage.rawData) {
-    Article.loadAll(JSON.parse(localStorage.rawData));
-    articleView.initIndexPage();
+    Project.loadAll(JSON.parse(localStorage.rawData));
+    projectView.initIndexPage();
   } else {
     $.getJSON('/data/projectIpsum.json')
     .then(function(rawData) {
-      Article.loadAll(rawData);
+      Project.loadAll(rawData);
       localStorage.rawData = JSON.stringify(rawData);
-      articleView.initIndexPage();
+      projectView.initIndexPage();
     }, function(err) {
       console.error(err);
     });
   }
 }
 
-// REVIEW: This new prototype method on the Article object constructor
-// will allow us to create a new article from the new.html form page,
-// and submit that data to the back-end. We will see this log out to the server
-// in our terminal!
-Article.prototype.insertRecord = function(callback) {
-  $.post('/articles', {author: this.author, authorUrl: this.authorUrl, body: this.body, category: this.category, publishedOn: this.publishedOn, title: this.title})
+Project.prototype.insertRecord = function(callback) {
+  $.post('/Projects', {author: this.author, authorUrl: this.authorUrl, body: this.body, category: this.category, publishedOn: this.publishedOn, title: this.title})
   .then(function(data) {
     console.log(data);
     if (callback) callback();
---
+++ public/scripts/projectView.js
@@ -0,0 +1,134 @@
+'use strict';
+
+const projectView = {};
+
+projectView.populateFilters = function() {
+  $('project').each(function() {
+    if (!$(this).hasClass('template')) {
+      let val = $(this).find('address a').text();
+      let optionTag = `<option value="${val}">${val}</option>`;
+
+      if ($(`#author-filter option[value="${val}"]`).length === 0) {
+        $('#author-filter').append(optionTag);
+      }
+
+      val = $(this).attr('data-category');
+      optionTag = `<option value="${val}">${val}</option>`;
+      if ($(`#category-filter option[value="${val}"]`).length === 0) {
+        $('#category-filter').append(optionTag);
+      }
+    }
+  });
+};
+
+projectView.handleAuthorFilter = function() {
+  $('#author-filter').on('change', function() {
+    if ($(this).val()) {
+      $('project').hide();
+      $(`project[data-author="${$(this).val()}"]`).fadeIn();
+    } else {
+      $('project').fadeIn();
+      $('project.template').hide();
+    }
+    $('#category-filter').val('');
+  });
+};
+
+projectView.handleCategoryFilter = function() {
+  $('#category-filter').on('change', function() {
+    if ($(this).val()) {
+      $('project').hide();
+      $(`project[data-category="${$(this).val()}"]`).fadeIn();
+    } else {
+      $('project').fadeIn();
+      $('project.template').hide();
+    }
+    $('#author-filter').val('');
+  });
+};
+
+projectView.handleMainNav = function() {
+  $('.main-nav').on('click', '.tab', function() {
+    $('.tab-content').hide();
+    $(`#${$(this).data('content')}`).fadeIn();
+  });
+
+  $('.main-nav .tab:first').click();
+};
+
+projectView.setTeasers = function() {
+  $('.project-body *:nth-of-type(n+2)').hide();
+  $('project').on('click', 'a.read-on', function(e) {
+    e.preventDefault();
+    if ($(this).text() === 'Read on →') {
+      $(this).parent().find('*').fadeIn();
+      $(this).html('Show Less &larr;');
+    } else {
+      $('body').animate({
+        scrollTop: ($(this).parent().offset().top)
+      },200);
+      $(this).html('Read on &rarr;');
+      $(this).parent().find('.project-body *:nth-of-type(n+2)').hide();
+    }
+  });
+};
+
+projectView.initNewprojectPage = function() {
+  $('.tab-content').show();
+  $('#export-field').hide();
+  $('#project-json').on('focus', function(){
+    this.select();
+  });
+
+  $('#new-form').on('change', 'input, textarea', projectView.create);
+  $('#new-form').on('submit', projectView.submit);
+};
+
+projectView.create = function() {
+  let project;
+  $('#projects').empty();
+
+  project = new project({
+    title: $('#project-title').val(),
+    author: $('#project-author').val(),
+    authorUrl: $('#project-author-url').val(),
+    category: $('#project-category').val(),
+    body: $('#project-body').val(),
+    publishedOn: $('#project-published:checked').length ? new Date() : null
+  });
+
+  $('#projects').append(project.toHtml());
+
+  $('pre code').each(function(i, block) {
+    hljs.highlightBlock(block);
+  });
+
+  $('#export-field').show();
+  $('#project-json').val(`${JSON.stringify(project)},`);
+};
+
+projectView.submit = function(event) {
+  event.preventDefault();
+  let project = new project({
+    title: $('#project-title').val(),
+    author: $('#project-author').val(),
+    authorUrl: $('#project-author-url').val(),
+    category: $('#project-category').val(),
+    body: $('#project-body').val(),
+    publishedOn: $('#project-published:checked').length ? new Date() : null
+  });
+
+  project.insertRecord();
+}
+
+projectView.initIndexPage = function() {
+  project.all.forEach(function(project){
+    $('#projects').append(project.toHtml())
+  });
+
+  projectView.populateFilters();
+  projectView.handleCategoryFilter();
+  projectView.handleAuthorFilter();
+  projectView.handleMainNav();
+  projectView.setTeasers();
+};
